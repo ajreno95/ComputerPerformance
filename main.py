f8bcfec6b560e06457f904bc5bca537688d2c175
Alexander Reno <ajreno952@gmail.com>
2017-11-08 14:12:06 -0500

    testing something new just creating a copy

--- main.py
+++ main.py
@@ -15,24 +15,35 @@ class PerformanceInformation:
             self.fans = psutil.sensors_fans()
         else:
             self.fans = None
+
+    def getVMInfo():
+        return psutil.virtual_memory()
         
         
 
 class MainForm(npyscreen.Form):
+    ThisPc = PerformanceInformation()
     def create(self):
-        ThisPc = PerformanceInformation()
-        self.CpuField = self.add(npyscreen.TitleText, name="CPU Count:\t", value = ThisPc.CPUCount, editable=False)
-        self.VmField = self.add(npyscreen.TitleText, name="Virtual Memory %:\t", value = ThisPc.VirtualMemory.percent, editable=False)
-        if(ThisPc.sensor == None):
+        self.keypress_timeout = 10
+        self.CpuField = self.add(npyscreen.TitleText, name="CPU Count:\t", value = MainForm.ThisPc.CPUCount, editable=False)
+        self.VmField = self.add(npyscreen.TitleText, name="Virtual Memory %:\t", value = MainForm.ThisPc.VirtualMemory.percent, editable=False)
+        if(MainForm.ThisPc.sensor == None):
             self.SensorField = self.add(npyscreen.TitleText, name="Sensors:", value = 'Cannot read sensors', editable=False)
         else:
-            self.SensorField = self.add(npyscreen.TitleText, name="Sensors:", value = ThisPc.sensor, editable=False)
-        if(ThisPc.fans == None):
+            self.SensorField = self.add(npyscreen.TitleText, name="Sensors:", value = MainForm.ThisPc.sensor, editable=False)
+        if(MainForm.ThisPc.fans == None):
             self.FanField = self.add(npyscreen.TitleText, name="Fans:", value = 'Cannot read fans', editable=False)
         else:
-            self.FanField = self.add(npyscreen.TitleText, name="Fans:", value = ThisPc.fans, editable=False)
+            self.FanField = self.add(npyscreen.TitleText, name="Fans:", value = MainForm.ThisPc.fans, editable=False)
+    
+    
+
     def afterEditing(self):
         self.parentApp.setNextForm(None)
+    
+    def while_waiting(self, ):
+        self.VmField = MainForm.ThisPc.getVMInfo
+        self.VmField.display()
         
         
 
